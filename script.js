740979aa37813b8b98f17b49549251d11efb33af
Idioxu <etai046@g.educaand.es>
2025-03-07 21:22:42 +0100

    como hago estpo para que funcione

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    function mostrarAlerta() {
+        alert("DIVINE PUNISHMENT :3");
+        terceraFuncion();
+        }
+    function segundafuncion() {
+        alert("XD")
+        }
+    function mostrarAlerta2() {
+    alert("I did wipe and lose my progress :´v ");
+    terceraFuncion();
+    }
+    function tercerafuncion() {
+    alert("You´re a permafreshie, you wiped :C");
+    }
